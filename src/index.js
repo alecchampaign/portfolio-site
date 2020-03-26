5fe1002afff7d480114f8e27233bd9e4c38e7493
Alec Champaign <alecchampaign@protonmail.com>
2020-03-26 17:31:55 -0400

    Begin Shop.ly project section

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './header';
+import Shoply from './shoply';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Shoply />
     </>
   );
 };
---
+++ src/shoply.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Shoply = () => (
+  <div id='shoply'>
+    <img src='./images/shoply.png'></img>
+    <div>
+      <a
+        class='title'
+        href='https://github.com/grey-octopus/project_greenfield'
+        target='blank'
+      >
+        Shop.ly
+      </a>
+      <p>
+        A dynamic product page for an e-commerce shopping platform, allowing
+        users to view product information, see related products, post reviews,
+        and ask/answer questions.
+      </p>
+      <a
+        class='github-btn'
+        href='https://github.com/grey-octopus/project_greenfield'
+        target='blank'
+      >
+        View GitHub
+      </a>
+    </div>
+  </div>
+);
+
+export default Shoply;
